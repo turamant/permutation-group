ab5cd3cc5e6c62e243a6283d3de7a8e889d03037
askvart <tur1amant@gmail.com>
2023-08-24 16:34:23 +0300

    fmt

--- main.go
+++ main.go
@@ -9,62 +9,59 @@ import (
 	"strings"
 )
 
-func init(){
+func init() {
 	buildDictionary()
 }
 
 var dictionary map[string][]string
 
-
-func alphabetize(word string) string{
+func alphabetize(word string) string {
 	s := strings.Split(word, "")
 	sort.Strings(s)
 	return strings.Join(s, "")
 }
 
-func buildDictionary(){
+func buildDictionary() {
 	dictionary = make(map[string][]string)
-	file,err := os.Open("words.txt")
-	
+	file, err := os.Open("words.txt")
+
 	if err != nil {
 		log.Printf("Error : %s", err)
 	}
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	var txtwords []string
-    
-	for scanner.Scan(){
+
+	for scanner.Scan() {
 		txtwords = append(txtwords, scanner.Text())
 	}
-	
+
 	file.Close()
-	for _, v := range txtwords{
+	for _, v := range txtwords {
 		fmt.Println(v)
 	}
-	for _, word := range txtwords{
-	 	alphabetized := alphabetize(word)
+	for _, word := range txtwords {
+		alphabetized := alphabetize(word)
 		var list []string
-		if len(dictionary) > 0 && len(dictionary[alphabetized]) > 0{
+		if len(dictionary) > 0 && len(dictionary[alphabetized]) > 0 {
 			list = dictionary[alphabetized]
-		}else{
+		} else {
 			list = []string{}
 		}
 		list = append(list, word)
 		dictionary[alphabetized] = list
-		
+
 	}
-	
+
 }
 
-func output(word string){
+func output(word string) {
 	wd := alphabetize(word)
 	fmt.Printf("Permutation group of %s is %s", word, dictionary[wd])
 
 }
 
-func main(){
+func main() {
 	output("esprits")
-	
-	
 
 }
